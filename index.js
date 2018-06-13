97cfa2efcdfca0408883198e650f1d4fae9fa67b
Vanessa Sheldon <vsheldon@me.com>
2018-06-13 17:22:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ console.log("Height:")
 console.log(parseInt(height) + 1)
 
 if (name == "Janet") {
-  console.log("Janet is")
-  console.log(height)
-  console.log("inches tall")
+  console.log("Janet is" height " inches tall")
 } else {
-  console.log("expected 'Janet' but got")
-  console.log(name)
-  console.log("instead")
+  console.log("expected 'Janet' but got" name " instead")
 }
